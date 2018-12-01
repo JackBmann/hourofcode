e30966103aa01f453c2275248cfb9b527150c088
Yeabkal <ywubshit@udallas.edu>
2018-11-30 19:26:34 -0600

    Update fibonacci.scala
    Add comments, improve nomenclature

--- fibonacci.scala
+++ fibonacci.scala
@@ -1,32 +1,42 @@
+/*
+ Computer Science Education Week, 2018
+ University of Dallas
+ Learn Scala!
+ Yeabkal Wubshit
+ */
 object Fibonacci {
   def main(args: Array[String]): Unit = {
-  	// Prompt user to enter the desired amount of
-  	// elements of the Fibonacci sequence to print.
+  	// Prompt user to enter the desired amount of Fibonacci numbers to print.
   	println("Enter the amount of Fibonacci Numbers you want to print")
 
-  	// Take a user input for the amount of Fibonacci numbers to print.
-  	val sizeOfFibonacciNumbers = scala.io.StdIn.readLine()
+  	// Take user input for the amount of Fibonacci numbers to print.
+  	val SizeOfFibonacciNumbers = scala.io.StdIn.readInt()
 
-  	// Store the Fibonacci numbers in an array.
-    val fibonacciNumbers = getFibonacciNumbers(sizeOfFibonacciNumbers)
+  	// Store the Fibonacci numbers in an array by calling the function
+    // returning an array of Fibonacci sequence.
+    val FibonacciNumbers = getFibonacciNumbers(SizeOfFibonacciNumbers)
 
-    // Print the elements of the Fibonacci numbers array.
-    for (i <- 0 to sizeOfFibonacciNumbers-1) {
-    	println(i + ": " + fibonacciNumbers(i))
+    // Print the elements of the array containing the Fibonacci numbers.
+    for (i <- 0 to SizeOfFibonacciNumbers-1) {
+    	println(i + ": " + FibonacciNumbers(i))
     }
   }
 
+  /*
+  n - number of Fibonacci numbers to calculate
+  returns an array containing 'n' Fibonacci numbers.
+  */
   def getFibonacciNumbers(n: Int) : Array[Int] = {
   	// Define the 1st and 2nd Fibonacci numbers.
-  	var fib_1_2 = 1
+  	val Fib_1_2 = 1
   	// Array to 'n' store Fibonacci numbers.
   	var fibNos = new Array[Int] (n)
   	// Set the first Fibonacci number.
-  	fibNos(0) = fib_1_2
+  	fibNos(0) = Fib_1_2
   	if(n > 1) { 
   		// Set the second Fibonacci number if the desired
   		// size is at least 2.
-  		fibNos(1) = fib_1_2
+  		fibNos(1) = Fib_1_2
   	}
   	// Fill the array of Fibonacci numbers using the formula:
   	// fib(n) = fib(n-1) + fib(n-2)
