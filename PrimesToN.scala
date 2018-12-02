9a8052893f6f44fe463c8b3f8f43d8332369c2fb
Yeabkal <ywubshit@udallas.edu>
2018-12-02 10:57:41 -0600

    Update PrimesToN.scala
    Add printing of result.

--- PrimesToN.scala
+++ PrimesToN.scala
@@ -2,9 +2,15 @@ object PrimesToN {
   def main(args: Array[String]): Unit = {
     val n = scala.io.StdIn.readInt()
     val primesToN = getPrimesToN(n)
+    for (i <- 0 to primesToN.length - 1) {
+      println(primesToN(i))
+    }
   }
 
   def getPrimesToN(n: Int) : List[Int] = {
     (2 to n).toList.filter(s=>(2 to scala.math.sqrt(s).toInt).toList.forall(g=>s%g!=0))
   } 
 }
+
+
+
