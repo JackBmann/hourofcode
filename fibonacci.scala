58cd7851ddd4b397069ade8390bd0316a92cf79d
Yeabkal <ywubshit@udallas.edu>
2018-12-03 16:40:26 -0600

    Update fibonacci.scala
    Add writing to file.

--- fibonacci.scala
+++ fibonacci.scala
@@ -4,46 +4,55 @@
  Learn Scala!
  Yeabkal Wubshit
  */
+import java.io._
+
 object Fibonacci {
   def main(args: Array[String]): Unit = {
-  	// Prompt user to enter the desired amount of Fibonacci numbers to print.
-  	println("Enter the amount of Fibonacci Numbers you want to print")
+    // Prompt user to enter the desired amount of Fibonacci numbers to print.
+    println("Enter the amount of Fibonacci Numbers you want to print:")
 
-  	// Take user input for the amount of Fibonacci numbers to print.
-   val SizeOfFibonacciNumbers = scala.io.StdIn.readInt()
+    // Take user input for the amount of Fibonacci numbers to print.
+    val SizeOfFibonacciNumbers = scala.io.StdIn.readInt()
 
-  	// Store the Fibonacci numbers in an array by calling the function
-   // returning an array of Fibonacci sequence.
-   val FibonacciNumbers = getFibonacciNumbers(SizeOfFibonacciNumbers)
+    // Store the Fibonacci numbers in an array by calling the function
+    // returning an array of Fibonacci sequence.
+    val FibonacciNumbers = getFibonacciNumbers(SizeOfFibonacciNumbers)
 
-   // Print the elements of the array containing the Fibonacci numbers.
-   for (i <- 0 to SizeOfFibonacciNumbers-1) {
-    println(i + ": " + FibonacciNumbers(i))
-   }
+    // Print the elements of the array containing the Fibonacci numbers.
+    // Also, write the Fibonacci numbers to a text file.
+    val pw = new PrintWriter(new File("fib_nums.txt"))
+    for (i <- 0 to SizeOfFibonacciNumbers - 1) {
+      val toPrint = i + ": " + FibonacciNumbers(i)
+      println(toPrint)
+      pw.write(toPrint + "\n")
+    }
+    // Close the print writer.
+    pw.close
   }
 
   /*
   n - number of Fibonacci numbers to calculate
   returns an array containing 'n' Fibonacci numbers.
-  */
-  def getFibonacciNumbers(n: Int) : Array[Int] = {
-  	// Define the 1st and 2nd Fibonacci numbers.
-  	val Fib_1_2 = 1
-  	// Array to 'n' store Fibonacci numbers.
-  	var fibNos = new Array[Int] (n)
-  	// Set the first Fibonacci number.
-  	fibNos(0) = Fib_1_2
-  	if(n > 1) { 
-  		// Set the second Fibonacci number if the desired
-  		// size is at least 2.
-  		fibNos(1) = Fib_1_2
-  	}
-  	// Fill the array of Fibonacci numbers using the formula:
-  	// fib(n) = fib(n-1) + fib(n-2)
-  	for(i <- 2 to n-1) {
-  		fibNos(i) = fibNos(i-1) + fibNos(i-2)
-  	}
-  	// Return the array of Fibonacci numbers
-  	fibNos
-  } 
+   */
+  def getFibonacciNumbers(n: Int): Array[Int] = {
+    // Define the 1st and 2nd Fibonacci numbers.
+    val Fib_1_2 = 1
+    // Array to 'n' store Fibonacci numbers.
+    var fibNos = new Array[Int](n)
+    // Set the first Fibonacci number.
+    fibNos(0) = Fib_1_2
+    if (n > 1) {
+      // Set the second Fibonacci number if the desired
+      // size is at least 2.
+      fibNos(1) = Fib_1_2
+    }
+    // Fill the array of Fibonacci numbers using the formula:
+    // fib(n) = fib(n-1) + fib(n-2)
+    for (i <- 2 to n - 1) {
+      fibNos(i) = fibNos(i - 1) + fibNos(i - 2)
+    }
+    // Return the array of Fibonacci numbers
+    fibNos
+  }
 }
+
