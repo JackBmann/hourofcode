fa96b47847a31853e43ff44c2511ea3b0b6d3989
plieblang <plieblang@users.noreply.github.com>
2018-12-03 16:43:44 -0600

    Arrays

--- fib.go
+++ fib.go
@@ -6,12 +6,17 @@ import (
 	"strconv"
 )
 
+var fibNumbers = make([]int, 0)
+
 func calculateFib(x int) int {
 	var prev, rv int = 1, 1
+	fibNumbers = append(fibNumbers, prev)
+	fibNumbers = append(fibNumbers, rv)
 	for i := 2; i < x; i++ {
 		old := rv
 		rv += prev
 		prev = old
+		fibNumbers = append(fibNumbers, rv)
 	}
 	return rv
 }
@@ -26,5 +31,6 @@ func main() {
 	defer file.Close()
 
 	fmt.Println(fib)
+	fmt.Println(fibNumbers)
 	file.WriteString(strconv.Itoa(fib))
 }
